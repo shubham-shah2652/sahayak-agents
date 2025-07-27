4f175873d90dd7c2e8caee6f59863bdf4f70de67
SanyaNanda <sanya.nanda@gmail.com>
2025-07-27 07:22:51 +0530

    agents

---
+++ performance_agents/grading_system/deployment/vertex_deployment.py
@@ -0,0 +1,29 @@
+from vertexai.preview import reasoning_engines
+from grading_system.sub_agents.agent import sequential_grader
+import vertexai
+from vertexai import agent_engines
+
+
+PROJECT_ID = "sahayakai-466115"
+LOCATION = "us-east4"
+STAGING_BUCKET = "gs://sahayak-grading-bucket"
+
+vertexai.init(
+    project=PROJECT_ID,
+    location=LOCATION,
+    staging_bucket=STAGING_BUCKET,
+)
+
+app = reasoning_engines.AdkApp(
+    agent=sequential_grader,
+    enable_tracing=True,
+)
+
+remote_app = agent_engines.create(
+    agent_engine=app,
+    requirements=[
+        "google-cloud-aiplatform[adk,agent_engines]", "cloudpickle", "google-adk", "PyPDF2", "opencv-python", "pillow", "google-cloud-firestore"
+    ],extra_packages=[
+        "./grading_system/sub_agents",
+    ],
+)
